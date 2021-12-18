e6da6671580a2e9e7b32ab1dde6c383ec39450a3
whsv26 <whsv26@gmail.com>
2021-12-19 00:20:16 +0300

    Fix parser psalm errors.

--- src/DependencyInjection/RoutingMapParser.php
+++ src/DependencyInjection/RoutingMapParser.php
@@ -2,7 +2,6 @@
 
 namespace Whsv26\Mediator\DependencyInjection;
 
-use Fp\Collections\Map;
 use Fp\Functional\Option\Option;
 use Fp\Streams\Stream;
 use PhpParser\Comment\Doc;
@@ -18,29 +17,31 @@ use Whsv26\Mediator\Contract\QueryHandlerInterface;
 use function Fp\classOf;
 use function Fp\Collection\firstOf;
 use function Fp\Collection\head;
+use function Fp\Evidence\proveNonEmptyString;
 use function Fp\Json\regExpMatch;
 
 /**
- * @psalm-type TQuery = class-string
- * @psalm-type TCommand = class-string
+ * @psalm-type TQuery = string
+ * @psalm-type TCommand = string
  * @psalm-type THandled = TQuery|TCommand
- * @psalm-type TQueryHandler = class-string
- * @psalm-type TCommandHandler = class-string
+ * @psalm-type TQueryHandler = string
+ * @psalm-type TCommandHandler = string
  * @psalm-type THandler = TQueryHandler|TCommandHandler
  */
 class RoutingMapParser
 {
     /**
-     * @return Map<THandled, THandler>
+     * @return array<THandled, THandler>
      */
-    public function parseDirRecursive(string $dir): Map
+    public function parseDirRecursive(string $dir): array
     {
         $directories = new RecursiveDirectoryIterator($dir);
         $files = new RecursiveIteratorIterator($directories);
         $phpFiles = new RegexIterator($files, '/^.+\.php$/i', RegexIterator::GET_MATCH);
 
         return Stream::emits($phpFiles)
-            ->filterMap(fn(string $path) => $this->parseFile($dir))
+            ->filterMap(fn($path) => proveNonEmptyString($path))
+            ->filterMap(fn(string $path) => $this->parseFile($path))
             ->toHashMap(fn(array $pair) => $pair)
             ->toAssocArray()
             ->get();
@@ -57,7 +58,9 @@ class RoutingMapParser
                 ->parse(file_get_contents($path));
 
             $tagRegExp = '/(?:@implements|@psalm-implements).*<.*,\s*(.*?)\s*>/';
-            $classStmt = yield firstOf($ast, Class_::class);
+
+            $classStmt = yield Option::fromNullable($ast)
+                ->flatMap(fn(array $stmts) => firstOf($stmts, Class_::class));
 
             yield Option::some($classStmt)
                 ->map(fn(Class_ $class) => $class->implements)
@@ -73,9 +76,9 @@ class RoutingMapParser
                 ->map(fn(Class_ $class) => $class->namespacedName)
                 ->map(fn(Name $name) => $name->toString());
 
-            $handledClass = Option::some($classStmt)
+            $handledClass = yield Option::some($classStmt)
                 ->flatMap(fn(Class_ $class) => Option::fromNullable($class->getDocComment()))
-                ->map(fn(Doc $doc) => $doc->getReformattedText())
+                ->map(fn(Doc $doc) => $doc->getText())
                 ->flatMap(fn(string $doc) => regExpMatch($doc, $tagRegExp, 1));
 
             return [$handledClass, $handlerClass];
